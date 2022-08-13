a1e3a8ae5c148027837b89c299c9ff233a721a8b
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-12 21:47:02 -0500

    Mostrando componente principal, si el presupuesto es valido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto';
 
-const Header = ({presupuesto, setPresupuesto}) => {
+const Header = ({presupuesto, setPresupuesto,setIsValidPresupuesto, isValidPresupuesto}) => {
     return ( 
         <header>
             <h1>Planificador de Gatos Personales</h1>
-            <NuevoPresupuesto
-                presupuesto={presupuesto}
-                setPresupuesto={setPresupuesto}
-            />
+            { isValidPresupuesto ? 
+            ( 
+                <h1>Presupuestos</h1> 
+            ) 
+            : 
+            (
+                <NuevoPresupuesto
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
+            )}
         </header>
      );
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,17 +1,19 @@
 import {Children, useState} from 'react';
 import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto,setIsValidPresupuesto}) => {
 
     const [mensaje , setMensaje] = useState('');
+    
 
     const handlePresupuesto = (e) => {
         e.preventDefault();
-        if(!Number(presupuesto) || Number(presupuesto) < 0 ){
+        if(!presupuesto || presupuesto < 0 ){
             setMensaje('No es un presupuesto valido');
-        }else{
-             setMensaje('Herew we go')
+            return
         }
+        setMensaje('');
+        setIsValidPresupuesto(true)
 
     }
 
@@ -22,11 +24,11 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                     <label > Definir Presupuesto</label>
 
                     <input 
-                        type="text"
+                        type="number"
                         className="nuevo-presupuesto" 
                         placeholder="Agrega tu presupuesto"
                         value={presupuesto}
-                        onChange={e => setPresupuesto(e.target.value)}
+                        onChange={e => setPresupuesto(Number(e.target.value))}
                     />
                 </div>
 
