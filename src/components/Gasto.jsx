8937913249ac477e67443686637c01c12f92bef5
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-24 23:27:55 -0500

    Mostrando fechas y agregando fecha y formato a la misma con un helper

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { formatearFecha } from '../helpers';
 
-export default function gasto() {
-  return (
-    <div>
-        <p>Desde Gasto</p>
-    </div>
-  )
+export default function gasto({gasto}) {
+    return (
+        <div className='gasto sombra ' >
+            <div className="contenido-gasto">
+                <div className="descripcion-gasto">
+                    <p className="categoria">
+                        {gasto.categoria}
+                    </p>
+                    <p className="nombre-gasto">
+                        {gasto.nombre }
+                    </p>
+                    <p className="fecha-gasto">Fecha: {''} 
+                    <span>{formatearFecha(gasto.fecha)}</span>  </p>
+                </div>
+            </div>
+                <p className="cantidad-gasto">${gasto.cantidad} </p>
+        </div>
+    )
 }
