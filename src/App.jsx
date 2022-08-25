e944d158e14c235d059c6913722f5148d35ee31a
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-25 00:55:32 -0500

    Calculando presupuesto gastado

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import ListadoGastos from './components/ListadoGastos';
 
 function App() {
 
+  const [gastos, setGastos] = useState([]);
+  
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal ] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
-  const [gastos, setGastos] = useState([]);
 
   const handleNuevoGasto = () =>{
     setModal(true);
@@ -38,6 +39,7 @@ function App() {
   return (
     <div className={modal ? 'fijar' : ''} > 
         <Header
+          gastos={gastos}
           presupuesto={presupuesto}
           setPresupuesto={setPresupuesto}
           isValidPresupuesto={isValidPresupuesto}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto';
 import ControlPresupuesto from './ControlPresupuesto';
 
-const Header = ({presupuesto, setPresupuesto,setIsValidPresupuesto, isValidPresupuesto}) => {
+const Header = ({presupuesto, setPresupuesto,setIsValidPresupuesto, isValidPresupuesto, gastos}) => {
     return ( 
         <header>
             <h1>Planificador de Gatos Personales</h1>
             { isValidPresupuesto ? 
             ( 
                 <ControlPresupuesto
+                    gastos={gastos}
                     presupuesto={presupuesto}
                 />
             ) 
