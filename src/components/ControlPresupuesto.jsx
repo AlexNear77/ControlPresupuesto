73d3e3262db3fbca0f4369df7ce3201f51f87fa7
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-27 00:08:37 -0500

    Calculando el presupuesto disponible

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -6,8 +6,12 @@ const ControlPresupuesto = ({gastos,presupuesto}) => {
     const [gastado, setGastado] = useState(0);
 
     useEffect(() => {
-       const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0 )
-       setGastado(totalGastado)
+        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0 );
+
+        const totalDisponible =  presupuesto - totalGastado;
+
+        setDisponible(totalDisponible);
+        setGastado(totalGastado);
     }, [gastos])
     
 
