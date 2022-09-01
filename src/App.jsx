78fabfaae881df93df9c638c8421a1d44dd9542d
Alejandro Gomez <alexgomeznear@gmail.comm>
2022-08-31 21:40:59 -0500

    Agregando presupuesto y gastos al localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,13 @@ import ListadoGastos from './components/ListadoGastos';
 
 function App() {
 
-  const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  );
   
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal ] = useState(false);
@@ -26,6 +30,24 @@ function App() {
         },400)
       }
   }, [gastoEditar])
+
+  useEffect(() => {
+      localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [presupuesto])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? []);
+  }, [gastos])
+  
+  
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if(presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+  }, [])
+  
   
 
   const handleNuevoGasto = () =>{
